262aa01774f325d6cf04c4f34ebf90abeed0eb88
Administrator <2314081385@qq.com>
2022-08-18 23:20:12 +0800

    项目经理完成了Two类的增加，完成了Student的修改

---
+++ git_001_javase/src/main/java/com/bjpowernode/Two.java
@@ -0,0 +1,7 @@
+package com.bjpowernode;
+
+public class Two {
+    public static void main(String[] args) {
+        System.out.println("这是项目经理新增加的类......");
+    }
+}
--- git_002_javaweb/src/main/java/com/bjpowernode/pojo/Student.java
+++ git_002_javaweb/src/main/java/com/bjpowernode/pojo/Student.java
@@ -3,5 +3,6 @@ package com.bjpowernode.pojo;
 public class Student {
     public static void main(String[] args) {
         System.out.println("javaWeb项目的内容......");
+        System.out.println("这是项目经理修改的Student中的内容......");
     }
 }
