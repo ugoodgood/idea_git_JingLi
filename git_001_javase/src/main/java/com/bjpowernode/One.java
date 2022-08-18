c274ff0930a1f40564adce744caf1ec7bbff9a3a
Administrator <2314081385@qq.com>
2022-08-18 22:48:05 +0800

    第一次项目仓库提交

---
+++ git_001_javase/src/main/java/com/bjpowernode/One.java
@@ -0,0 +1,7 @@
+package com.bjpowernode;
+
+public class One {
+    public static void main(String[] args) {
+        System.out.println("这是javaSE项目的功能......");
+    }
+}
---
+++ git_002_javaweb/src/main/java/com/bjpowernode/pojo/Student.java
@@ -0,0 +1,7 @@
+package com.bjpowernode.pojo;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("javaWeb项目的内容......");
+    }
+}
